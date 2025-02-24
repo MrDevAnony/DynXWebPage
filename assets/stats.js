4b80af9628664ba5ba74b86dc202597aee41cf2f
MrDevAnony <mrdevanony@gmail.com>
2025-02-25 01:01:33 +0330

    debug and fix

---
+++ assets/stats.js
@@ -0,0 +1,15 @@
+async function fetchDnsStats() {
+    try {
+      const response = await fetch('https://api.dynx.pro/dns-stats');
+      if (!response.ok) throw new Error('Failed to fetch stats');
+  
+      const data = await response.json();
+      document.getElementById('total-queries').textContent = data.num_dns_queries.toLocaleString('en-US');
+      document.getElementById('blocked-queries').textContent = data.num_blocked_filtering.toLocaleString('en-US');
+    } catch (error) {
+      console.error(error);
+      document.getElementById('total-queries').textContent = 'Error';
+      document.getElementById('blocked-queries').textContent = 'Error';
+    }
+  }
+  fetchDnsStats();  
